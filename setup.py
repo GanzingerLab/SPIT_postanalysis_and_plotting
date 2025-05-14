00c94966c7f56c0591133cb033196f1236938eb4
gcastrolinares <Gerry.7914@gmail.com>
2025-05-14 16:49:08 +0200

    setting up

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'postSPIT',
+    description = 'Package used to do post-processing of SPIT results an to plot the data genereated by SPIT and by this same package',
+    version = '0.1',
+    packages = find_packages(),
+    author= 'Gerard Castro-Linares',
+    url='',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+   ],
+   
+)
